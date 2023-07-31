d68532d9564c75b3d2f3a42bdfa58bc07fe2d5fe
“Afarisphi” <agung.afr10@gmail.com>
2023-07-31 09:46:04 +0700

    edit file html

---
+++ pertemuan 2 Nilai dan Tipe Data/script.js
@@ -0,0 +1,25 @@
+// // Alert JavaScript
+// alert("Hallo Dunia");
+
+// // Prompt JavaScript
+// let x = prompt ('masukan nama kamu');
+// alert ('Hallo ' + x);
+
+// Confirm JavaScript
+// let y = confirm ('kamu yakin mbak?');
+// alert(y);
+// if (y === true) {
+//   alert ('user memilih OK');
+// } else {
+//   alert ('user memilih TIDAK');
+// }
+
+alert('Selamat Datang..');
+let z;
+
+while (z) {
+  let x = prompt('masukan nama kamu');
+  alert('Selamat Datang ' + x);
+  z = confirm('masukan nama lagi?');
+}
+alert('Terima Kasih');
